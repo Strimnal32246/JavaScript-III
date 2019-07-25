902dd42ddd62ec5639892d5f84125f3f4f02d8e4
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-07-24 21:36:53 -0400

    added rest of material from peer group that we had gone over

--- assignments/this.js
+++ assignments/this.js
@@ -1,18 +1,22 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1. Global -  global scope is refering to the window object.
+ * 2. Implict -  left of the dot
+ * 3. New binding -is refering to an object created by a function.
+ * 4. Explict binding - whenever.cal(), .apply(), is applied it hijacks
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
-
+// Window Binding
+function yellName(susan) {
+    console.log(this);
+    return susan
+}
 // code example for Window Binding
-
+console.log(yellName('SusanTrimnal!!!'))
 // Principle 2
 
 // code example for Implicit Binding
